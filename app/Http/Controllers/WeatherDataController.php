fae53044f424887c9fdcf61fad684af91917d735
quentin.lagadec <quentin.lagadec@info13e10.ipa.iutlens.local>
2023-01-05 12:17:05 +0100

    Routes

--- app/Http/Controllers/WeatherDataController.php
+++ app/Http/Controllers/WeatherDataController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CurlService;
 use Illuminate\Http\Request;
 
 class WeatherDataController extends Controller
 {
-    public function search(){
+    public function index(){
         $curlService = new CurlService();
         $reponse = $curlService->get('https://wttr.in/');
     }
