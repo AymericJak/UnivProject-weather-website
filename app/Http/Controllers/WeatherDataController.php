a53a6c74a55331e3105aa649b6a4e4cf6ad6c758
AymericJak <aymericjakpro@gmail.com>
2023-01-05 12:18:55 +0100

    Changements controller

--- app/Http/Controllers/WeatherDataController.php
+++ app/Http/Controllers/WeatherDataController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\CurlService;
 
-class WeatherDataController extends Controller
-{
-    public function search(){
+class WeatherDataController extends Controller {
+
+    public function search() {
         $curlService = new CurlService();
         $reponse = $curlService->get('https://wttr.in/');
+    }
+
+    public function get(string $url) {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,// your preferred link
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_TIMEOUT => 30000,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                // Set Here Your Requesred Headers
+                'Content-Type: application/json',
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        if (!$response) {
+            return json_decode($err);
+        }
+
+        return $response;
     }
 }
