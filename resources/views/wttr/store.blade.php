222f7942aeb4561eafff2b4c1e6fe10914619a12
quentin.lagadec <quentin.lagadec@info12e05.ipa.iutlens.local>
2023-01-12 12:48:09 +0100

    Style page Recherche et Valeur

---
+++ resources/views/wttr/store.blade.php
@@ -0,0 +1,37 @@
+<head>
+    <link rel="stylesheet" href="storage/css/app.css" />
+    <title>
+        Site Api Weather
+    </title>
+</head>
+<body>
+<header>
+    <nav>
+        <h1>Bienvenue sur notre site api weather</h1>
+    </nav>
+</header>
+
+<table>
+    <tr>
+        <td>Nom de la ville</td>
+        <td>Température</td>
+        <td>Pression</td>
+        <td>Humidité</td>
+    </tr>
+    <tr>
+        <td>Lens</td>
+        <td>14°C</td>
+        <td>15</td>
+        <td>70%</td>
+    </tr>
+
+</table>
+
+
+<footer>
+    <p>Aymeric Jakobowski 2B<br>
+        Quentin Lagadec 2B</p>
+</footer>
+
+</body>
+</html>
